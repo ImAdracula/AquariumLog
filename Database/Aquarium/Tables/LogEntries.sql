e802c60dcaaa3e48bb8a2b4d367ff93be0a5dae6
Timothy Stepanski <tstepanski@gaig.com>
2017-05-03 16:17:58 -0400

    added pH with constraint to log entries table

--- Database/Aquarium/Tables/LogEntries.sql
+++ Database/Aquarium/Tables/LogEntries.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[LogEntries]
 (
 	[Id] INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
-	[TimeStamp] DATETIME NOT NULL
+	[TimeStamp] DATETIME NOT NULL,
+	[PotentialOfHydrogen] TINYINT NULL, 
+    CONSTRAINT [CK_LogEntries_PotentialOfHydrogenIs14OrLess] CHECK ([PotentialOfHydrogen] IS NULL OR [PotentialOfHydrogen] <= 14)
 )
 
 GO
